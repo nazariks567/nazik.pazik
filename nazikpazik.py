0311f7368e88dc243a2c2a2c733012a7ebcb2985
nazariks567 <153528322+nazariks567@users.noreply.github.com>
2023-12-11 17:01:51 +0200

    add file vwtch program

---
+++ nazikpazik.py
@@ -0,0 +1,17 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+
+app=QApplication([])
+win=QWidget()
+
+win.resize(500,500)
+win.setWindowTitle("Tест ГібХаб")
+
+title=QLabel("Тестовий додаток для гіт хабу ")
+
+line_h=QHBoxLayout()
+line_h.addWidget(title)
+win.setLayout(line_h)
+
+win.show()
+app.exec_()
